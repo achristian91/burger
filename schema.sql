c554324e9d6bbf7ed59df61ea9ae17d7a1854cae
Arthur Christian <32134423+achristian91@users.noreply.github.com>
2018-11-27 10:41:05 -0500

    added burger models

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(200) Not NULL,
+    burger_name VARCHAR(250) Not NULL,
     eaten BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
