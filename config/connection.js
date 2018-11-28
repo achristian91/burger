b00331543d0f1f8de4bdb2796644259e47b53de6
Arthur Christian <32134423+achristian91@users.noreply.github.com>
2018-11-28 00:27:05 -0500

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,14 @@
 var mysql = require('mysql');
-var connection;
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'wilkinson',
+    database:'burgers_db'
+})
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'wilkinson',
-        database: 'burgers_db'
-    });
-};
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id: " + connection.threadId);
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("connected as id: "+ connection.threadId);
 });
 
-connection.connect();
 module.exports = connection;
