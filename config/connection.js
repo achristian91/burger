0bd6d5988ea48c7487947993fbc5c95dcc4439e1
Arthur Christian <32134423+achristian91@users.noreply.github.com>
2018-11-28 20:05:25 -0500

    Remove the now ignored directory node_modules

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'wilkinson',
-    database:'burgers_db'
-})
+var connection;
 
-connection.connect(function(err){
-    if(err)throw err;
-    console.log("connected as id: "+ connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'wilkinson',
+        database: 'burgers_db'
+    });
+};
 
+// connection.connect(function(err){
+//     if(err)throw err;
+//     console.log("connected as id: "+ connection.threadId);
+// });
+
+connection.connect();
 module.exports = connection;
